e02ccac2826a21870ee4bb7727d1870008d983d1
Petro Kulybaba <ahurtep@gmail.com>
2019-01-17 13:51:52 +0200

    Added button (back to top)

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     $('#link').tooltip({ placement: "bottom" });
+    $('#back-to-top').tooltip({ placement: "right" });
+
+    $('#back-to-top').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 1500);
+        return false;
+    });
 });
